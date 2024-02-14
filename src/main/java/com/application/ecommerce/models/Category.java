e5ef8d65f7c4bf529994d50b1aff5e84ce008116
Alejandro Rey Vera <alejoreyv@gmail.com>
2024-02-13 20:12:36 -0500

    Subo cambios a mi rama, relacionados con Product y Caregory

---
+++ src/main/java/com/application/ecommerce/models/Category.java
@@ -0,0 +1,27 @@
+package com.application.ecommerce.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Setter
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "categoria")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nombre")
+    private String name;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, fetch = FetchType.LAZY,orphanRemoval = true)
+    private List<Product> productList = new ArrayList<>();
+}
