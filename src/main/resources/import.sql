a13bca0dfe76a258659e86fb9e20a8e98e924ba0
CodeLuDev <lucyromano12@gmail.com>
2024-02-16 18:49:26 -0600

    he agregado la entidad de orden y tambien sus insert para la base de datos

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -10,3 +10,6 @@ INSERT INTO producto(id, nombre, precio, id_categoria) VALUES(2, 'Asiento acolch
 INSERT INTO producto(id, nombre, precio, id_categoria) VALUES(3, 'Llanta de bicicleta de montaña', 150.99, 3);
 INSERT INTO producto(id, nombre, precio, id_categoria) VALUES(4, 'Freno de disco hidráulico', 185, 4);
 INSERT INTO producto(id, nombre, precio, id_categoria) VALUES(5, 'Pedales de plataforma', 50, 5);
+
+INSERT INTO orden(id, estado) VALUES(1, 'Creado');
+INSERT INTO orden(id, estado) VALUES (2, 'Orden recogida');
