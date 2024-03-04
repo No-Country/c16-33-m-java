bc8cfc7c92e543b22e16c02e4d43417c41de4a7c
Alejandro Rey Vera <alejoreyv@gmail.com>
2024-03-03 21:00:11 -0500

    Cambios Corregidos, Subire a mi rama alejandro, que tende cambios en la Fron y el Back

--- backend/src/main/java/com/application/ecommerce/service/impl/CartServiceImpl.java
+++ backend/src/main/java/com/application/ecommerce/service/impl/CartServiceImpl.java
@@ -1,12 +1,11 @@
 package com.application.ecommerce.service.impl;
 
 import com.application.ecommerce.entities.Cart;
-<<<<<<< HEAD
-=======
+
 import com.application.ecommerce.models.CartPK;
 import com.application.ecommerce.models.GetCartProductsResponse;
 import com.application.ecommerce.models.ProductWithCartDetails;
->>>>>>> 041dd2b88d9ee84086039e997c99d51d657d59f3
+
 import com.application.ecommerce.persistence.ICartDAO;
 import com.application.ecommerce.service.ICartService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,16 +20,6 @@ public class CartServiceImpl implements ICartService {
 
     @Autowired
     private ICartDAO cartDAO;
-<<<<<<< HEAD
-
-    @Override
-    public List<Cart> findAll() {
-        return cartDAO.findAll();
-    }
-
-    @Override
-    public Optional<Cart> findById(Long id) {
-=======
     @Override
     public GetCartProductsResponse findAll(Long customerId) {
         List<ProductWithCartDetails> products = cartDAO.getAllProducts(customerId);
@@ -44,36 +33,17 @@ public class CartServiceImpl implements ICartService {
 
     @Override
     public Optional<Cart> findById(CartPK id) {
->>>>>>> 041dd2b88d9ee84086039e997c99d51d657d59f3
         return cartDAO.findById(id);
     }
 
     @Override
-<<<<<<< HEAD
-    public Optional<Cart> findCartByIdOrder(Long idOrder) {
-        return cartDAO.findCartByIdOrder(idOrder);
-    }
-
-    @Override
-    public Optional<Cart> findCartByTotal(BigDecimal total) {
-        return cartDAO.findCartByTotal(total);
-    }
-
-    @Override
-    public Cart saveCart(Cart cart) {
-=======
     public Cart saveCart(Cart cart) {
         cart.setCartPK(new CartPK(cart.getCustomer().getId(), cart.getProduct().getId()));
->>>>>>> 041dd2b88d9ee84086039e997c99d51d657d59f3
         return cartDAO.saveCart(cart);
     }
 
     @Override
-<<<<<<< HEAD
-    public void deleteCartById(Long id) {
-=======
     public void deleteCartById(CartPK id) {
->>>>>>> 041dd2b88d9ee84086039e997c99d51d657d59f3
         cartDAO.deleteCartById(id);
     }
 }
